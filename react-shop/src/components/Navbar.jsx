a439b956ce89dbe5a83d7c1a9abdaf814a02ff8a
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-03-12 11:50:48 +0400

    navbar

--- react-shop/src/components/Navbar.jsx
+++ react-shop/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-//import styled from 'styled-components'
-const Container = styled
+import styled from 'styled-components'
+const Container = styled.div`
+    height: 60px;
+    background-color: black;
+`
 
 const Navbar = () => {
     return (
-        <div>
+        <Container>
             Navbar
-        </div>
+        </Container>
     )
 }
 
