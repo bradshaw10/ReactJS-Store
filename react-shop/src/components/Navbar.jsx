e3ec6665184d264646ea52afac160d96f88885a9
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-03-14 21:29:40 +0400

    Update Navbar.jsx

--- react-shop/src/components/Navbar.jsx
+++ react-shop/src/components/Navbar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 const Container = styled.div`
     height: 60px;
-    background-color: black;
+`
+
+const Wrapper = styled.div`
+    padding: 10px;
 `
 
 const Navbar = () => {
